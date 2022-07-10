5738414c9cec7740834a0f71d743a0f081ee16ac
nguyenhaininh <hulkhulk1245@gmai.com>
2022-07-10 14:27:04 +0700

    quản lý JE

--- src/main/java/com/itsol/recruit/repository/UserRepository.java
+++ src/main/java/com/itsol/recruit/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.itsol.recruit.repository;
 
 import com.itsol.recruit.entity.User;
 import com.itsol.recruit.repository.repoext.UserRepositoryExt;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +15,7 @@ import java.util.Optional;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryExt {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryExt, JpaSpecificationExecutor<User> {
 
 
     User findByUserName(String userName);
@@ -19,4 +24,5 @@ public interface UserRepository extends JpaRepository<User, Long>, UserRepositor
     User findUserByEmail(String email);
     User findUserByPhoneNumber(String phone);
     Optional<User> findByEmail(String username);
+
 }
--- src/main/java/com/itsol/recruit/repository/repoext/UserRepositoryExt.java
+++ src/main/java/com/itsol/recruit/repository/repoext/UserRepositoryExt.java
@@ -1,6 +1,7 @@
 package com.itsol.recruit.repository.repoext;
 
 import com.itsol.recruit.entity.User;
+import com.itsol.recruit.repository.UserRepository;
 
 public interface UserRepositoryExt {
     public User getAllUser();
